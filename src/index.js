709ad498e7f9576afa10992ac21c564555356b1f
yaoleksa <mryaremchyk@gmail.com>
2025-05-17 15:07:45 +0300

    started work with main document front page

--- src/index.js
+++ src/index.js
@@ -20,23 +20,23 @@ function QRcode() {
     return (
         <svg className='qr' width="26" height="22" viewBox="0 0 120 120" xmlns="http://www.w3.org/2000/svg">
             
-            <line x1="10" y1="10" x2="30" y2="10" stroke="black" stroke-width="4.5" />
-            <line x1="10" y1="10" x2="10" y2="30" stroke="black" stroke-width="4.5" />
+            <line x1="10" y1="10" x2="30" y2="10" stroke="black" strokeWidth="4.5" />
+            <line x1="10" y1="10" x2="10" y2="30" stroke="black" strokeWidth="4.5" />
 
             
-            <line x1="90" y1="10" x2="110" y2="10" stroke="black" stroke-width="4.5" />
-            <line x1="110" y1="10" x2="110" y2="30" stroke="black" stroke-width="4.5" />
+            <line x1="90" y1="10" x2="110" y2="10" stroke="black" strokeWidth="4.5" />
+            <line x1="110" y1="10" x2="110" y2="30" stroke="black" strokeWidth="4.5" />
 
             
-            <line x1="10" y1="90" x2="10" y2="110" stroke="black" stroke-width="4.5" />
-            <line x1="10" y1="110" x2="30" y2="110" stroke="black" stroke-width="4.5" />
+            <line x1="10" y1="90" x2="10" y2="110" stroke="black" strokeWidth="4.5" />
+            <line x1="10" y1="110" x2="30" y2="110" stroke="black" strokeWidth="4.5" />
 
             
-            <line x1="110" y1="90" x2="110" y2="110" stroke="black" stroke-width="4.5" />
-            <line x1="90" y1="110" x2="110" y2="110" stroke="black" stroke-width="4.5" />
+            <line x1="110" y1="90" x2="110" y2="110" stroke="black" strokeWidth="4.5" />
+            <line x1="90" y1="110" x2="110" y2="110" stroke="black" strokeWidth="4.5" />
 
             
-            <text x="60" y="73" font-size="44" font-family="sans-serif" text-anchor="middle" fill="black">
+            <text x="60" y="73" fontSize="44" fontFamily="sans-serif" textAnchor="middle" fill="black">
                 QR
             </text>
         </svg>
@@ -59,9 +59,17 @@ function Head() {
     );
 }
 
+function MainDocument() {
+    return (<div className='main-document'>
+        <p className='main-document-name'>Військово-обліковий документ</p>
+        <img src="./imgs/sign.png" />
+    </div>);
+}
+
 function Page() {
     return (<>
         <Head />
+        <MainDocument />
     </>);
 }
 
